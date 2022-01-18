eba585e051c1a6469598fbdeb9cd68b123951f11
Arif Dikkanat <61694750+ArifDKNT@users.noreply.github.com>
2022-01-19 02:23:07 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Transactions from './components/transactions/transactions';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App has-background-light">
+      <div className="container-is-fluent">
+          <div className="columns">
+            <div className="column">
+              <Transactions></Transactions>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
